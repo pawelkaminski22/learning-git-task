8afa4a81f70719a0b0f46b35e74c03b12268143f
Pawel Kaminski <pawelkaminski22@gmail.com>
2020-08-24 22:26:32 +0200

    Add list

---
+++ main.py
@@ -0,0 +1,6 @@
+sklepy = {
+'piekarnia': ['chleb', 'pączek', 'bułki'],
+'warzywniak': ['marchew', 'seler', 'rukola'],
+'lidl': ['czekolada', 'papier', 'szmpon']
+}
+count = sum([len(value) for value in sklepy.values()])
