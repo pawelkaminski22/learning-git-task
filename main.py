564981ae327548009548a39bf2270d5544aef9ed
Pawel Kaminski <pawelkaminski22@gmail.com>
2020-08-24 22:27:46 +0200

    Add for and print

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ sklepy = {
 'lidl': ['czekolada', 'papier', 'szmpon']
 }
 count = sum([len(value) for value in sklepy.values()])
+
+for sklep in sklepy:
+    print("Idę do", sklep.capitalize() , "i kupuję tam:", ', '.join([s.capitalize() for s in sklepy.get(sklep)]))
+
+print("W sumie kupuję", count, "produktów")
+
+#a = [11,122,12,32,10,43,23,53,123,54,24]
+#print(' > '.join([str(ae) for ae in a]))
