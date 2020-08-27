cc80c5e48649e3521002c1e1a1815779478aefa7
Pawel Kaminski <pawelkaminski22@gmail.com>
2020-08-27 13:57:40 +0200

    add print

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ for sklep in sklepy:
     print("Idę do", sklep.capitalize() , "i kupuję tam:", ', '.join([s.capitalize() for s in sklepy.get(sklep)]))
 
 print("W sumie kupuję", count, "produktów")
+print('drugi commit')
 
 #a = [11,122,12,32,10,43,23,53,123,54,24]
 #print(' > '.join([str(ae) for ae in a]))
