8fecb504a109ba199faafada3bba1fe5c41ba8c1
Pawel Kaminski <pawelkaminski22@gmail.com>
2020-08-31 13:51:19 +0200

    from pycharm

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ for sklep in sklepy:
 print("W sumie kupuję", count, "produktów")
 print('pierwszy commit')
 print('drugi commit')
+print('text z pycharm')
 
 #a = [11,122,12,32,10,43,23,53,123,54,24]
 #print(' > '.join([str(ae) for ae in a]))
